5ce59ca2befdeaca8c2d70d8b532ce952fab4832
pit4nya <pit4nya@gmail.com>
2016-07-26 11:20:10 +0900

    temp code

--- BMS/src/mainFrame.java
+++ BMS/src/mainFrame.java
@@ -3,7 +3,10 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Vector;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -19,7 +22,8 @@ public class mainFrame extends JFrame {
 
 	private JPanel contentPane;
 	Toolkit tk = Toolkit.getDefaultToolkit();
-
+	JFileChooser fileChooser = new JFileChooser();
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -39,15 +43,28 @@ public class mainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1700, 1000);
 		setLocation((tk.getScreenSize().width - getWidth()) / 2, (tk.getScreenSize().height - getHeight()) / 2);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[933.00px,grow][589px]", "[47px][479px,grow]"));
 
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-
+		
 		JMenu file_Menu = new JMenu("File");
 		menuBar.add(file_Menu);
 
-		JMenuItem refresh_Menu = new JMenuItem("Refresh");
-		file_Menu.add(refresh_Menu);
+		JMenuItem open_Menu = new JMenuItem("Open DataBase");
+		file_Menu.add(open_Menu);
+		
+		// 여기 더 구현해야 함
+		open_Menu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileChooser.setCurrentDirectory(new File("D:\\"));
+				int result = fileChooser.showOpenDialog(contentPane);
+			}
+		});
 
 		JMenuItem exit_Menu = new JMenuItem("Exit");
 		file_Menu.add(exit_Menu);
@@ -56,15 +73,15 @@ public class mainFrame extends JFrame {
 				System.exit(0);
 			}
 		});
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[933.00px,grow][589px]", "[47px][479px,grow]"));
-
+		
+		/////////////////////// filechooser button 만들어서 listener 안에 넣으면 됨
+		
 		JPanel name_Panel = new JPanel();
 		name_Panel.setLayout(null);
 		name_Panel.setBounds(100, 100, 200, 200);
 		contentPane.add(name_Panel, "cell 0 0,grow");
+		
+		
 
 		JLabel label = new JLabel("도서관리프로그램");
 		label.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 28));
@@ -81,41 +98,41 @@ public class mainFrame extends JFrame {
 		// 이건 오른쪽에 정보열람 tab 생성
 		tabbedPanel_Right_infoView tabpR_iV = new tabbedPanel_Right_infoView("정보열람");
 
-		refresh_Menu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == refresh_Menu) {
-					Vector title_Book = new Vector();
-					Vector data_Book = new Vector();
-					title_Book.add("도서번호");
-					title_Book.add("도서명");
-					title_Book.add("저자");
-					title_Book.add("출판사");
-
-					DefaultTableModel model_Book = new DefaultTableModel() {
-						public boolean isCellEditable(int row, int column) {
-							if (column >= 0) {
-								return false;
-							} else {
-								return true;
-							}
-						}
-					};
-
-					tabpL.book_table.setModel(model_Book);
-
-					readExcel readEx = new readExcel();
-
-					data_Book = readEx.getVector();
-
-					// JTable에 붙임
-					model_Book.setDataVector(data_Book, title_Book);
-					tabpL.scrollPane_Book.setViewportView(tabpL.book_table);
-
-				}
-
-			}
-
-		});
+		// 나중에 아래 버튼에 리스너 추가
+//		refresh_Menu.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				if (e.getSource() == refresh_Menu) {
+//					refresh_Menu.setSelected(false);
+//					Vector title_Book = new Vector();
+//					Vector data_Book = new Vector();
+//					title_Book.add("도서번호");
+//					title_Book.add("도서명");
+//					title_Book.add("저자");
+//					title_Book.add("출판사");
+//					
+//					DefaultTableModel model_Book = new DefaultTableModel() {
+//						public boolean isCellEditable(int row, int column) {
+//							if (column >= 0) {
+//								return false;
+//							} else {
+//								return true;
+//							}
+//						}
+//					};
+//					tabpL.book_table.setModel(model_Book);
+//					
+//					readExcel readEx = new readExcel();
+//					data_Book = readEx.getVector();
+//
+//					// JTable에 붙임
+//					model_Book.setDataVector(data_Book, title_Book);
+//					tabpL.scrollPane_Book.setViewportView(tabpL.book_table);
+//
+//				}
+//
+//			}
+//
+//		});
 
 		// 최상위 Panel에 생성 한거 가져다 붙임
 		contentPane.add(tabpL.getPanel(), "cell 0 1,grow");
